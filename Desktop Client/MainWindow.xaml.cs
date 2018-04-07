17bee43e90488e778320f3bdebe187938f80bfeb
ames1mp <ames1mp@gmail.com>
2018-04-07 05:01:22 -0400

    first commit

---
+++ Desktop Client/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Desktop_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Messages messagePage;
+        Connection connectionPage;
+        Devices devicesPage;
+
+        public Connection ConnectionPage { get => connectionPage; set => connectionPage = value; }
+
+        public MainWindow()
+        {
+            messagePage = new Messages();
+            connectionPage = new Connection();
+            devicesPage = new Devices();
+            InitializeComponent();
+        }
+
+        private void messageButton_click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = messagePage;
+        }
+
+
+        private void connectionButton_click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = connectionPage;
+        }
+
+        private void devicesButton_click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = devicesPage;
+        }
+    }
+}
---
+++ Desktop Client/MessageStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop_Client
+{
+    class MessageStore
+    {
+        public List<String> messages { get; set; }
+
+        public MessageStore()
+        {
+            messages = new List<String>();
+        }
+
+        public static void writeInfo(string message) {
+
+            System.IO.File.WriteAllText(@"C:\Users\Mike\Desktop\WriteLines.txt", message);
+        }
+    }
+}
